7e72d56524e3e6f01e863aaa581f299a0874d502
2Tough <vainferro@gmail.com>
2022-12-08 12:55:19 +0300

    Entered a couple of functions.

--- tetris.py
+++ tetris.py
@@ -32,9 +32,9 @@ top_left_y = s_height - play_height
 # SHAPE FORMATS
  
 S = [[\'.....\',
-      \'......\',
-      \'..00..\',
-      \'.00...\',
+      \'.....\',
+      \'..00.\',
+      \'.00..\',
       \'.....\'],
      [\'.....\',
       \'..0..\',
@@ -139,10 +139,22 @@ shape_colors = [(0, 255, 0), (255, 0, 0), (0, 255, 255), (255, 255, 0), (255, 16
  
  
 class Piece(object):
-    pass
+    def __init__(self, x, y, shape):
+        self.x = x
+        self.y = y
+        self.shape = shape
+        self.color = shape_colors[shapes.index(shape)]
+        self.rotation = 0
  
 def create_grid(locked_positions={}):
-    pass
+    grid = [[(0,0,0) for x in range(10)] for _ in range(20)]
+        for j in range(len(grid[i])):
+            if(j,i) in locked_pos:
+                c = locked_pos[(j,s)]
+                grid[i][j] = c
+
+        return grid
+
  
 def convert_shape_format(shape):
     pass
@@ -153,16 +165,27 @@ def valid_space(shape, grid):
 def check_lost(positions):
     pass
  
-def get_shape():
-    pass
+def get_shape(shapes):
+    return random.choice()
  
  
 def draw_text_middle(text, size, color, surface):
     pass
    
-def draw_grid(surface, row, col):
-    pass
- 
+def draw_grid(surface, grid):
+    surface.fill((0,0,0))
+    
+    pygame.font.init()
+    font = pygame.font.SysFont('comicsans', 60)
+    label = font.render('Tetris', 1, (255, 255, 255))
+
+    surface.blit(label, (top_left_x + play_width/2 - label.get_width()/2, 30))
+
+    for i in range(len(grid)):
+        for j in range(len(grid[i])):
+            pygame.draw.rect(surface, grid[i][j], (top_left_x + j*30, top_left_y + i*30)
+
+
 def clear_rows(grid, locked):
  
  
