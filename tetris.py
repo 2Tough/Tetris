8bcce898bcf1cb50664c84232b147b79f00bff2e
2Tough <vainferro@gmail.com>
2022-12-11 20:02:55 +0300

    Update tetris.py

--- tetris.py
+++ tetris.py
@@ -173,18 +173,15 @@ def draw_text_middle(text, size, color, surface):
     pass
    
 def draw_grid(surface, grid):
-    surface.fill((0,0,0))
-    
-    pygame.font.init()
-    font = pygame.font.SysFont('comicsans', 60)
-    label = font.render('Tetris', 1, (255, 255, 255))
-
-    surface.blit(label, (top_left_x + play_width/2 - label.get_width()/2, 30))
+   
 
     for i in range(len(grid)):
         for j in range(len(grid[i])):
             pygame.draw.rect(surface, grid[i][j], (top_left_x + j*block_size, top_left_y + i*block_size, block_size, block_size, 0)
 
+    pygame.draw.rect(surface, (255, 0, 0), (top_left_x, top_left_y, play_width, play_height), 4)            
+
+    pygame.display.update()
 
 def clear_rows(grid, locked):
  
@@ -192,11 +189,30 @@ def clear_rows(grid, locked):
 def draw_next_shape(shape, surface):
  
  
-def draw_window(surface):
-    pass
+def draw_window(surface, grid):
+     surface.fill((0,0,0))
+    
+    pygame.font.init()
+    font = pygame.font.SysFont('comicsans', 60)
+    label = font.render('Tetris', 1, (255, 255, 255))
+
+    surface.blit(label, (top_left_x + play_width/2 - label.get_width()/2, 30))
+    
+    draw_grid(surface, grid)
+    pygame.display.update()
  
 def main():
-    pass
+    
+    locked_positions = {}
+    grid = create_grid(locked_positions)
+
+    change_piece = False
+    run = True
+    current_piece = get_shape()
+    next_piece = get_shape()
+    next_piece = get_shape()
+    clock = pygame.time.Clock()
+    fall_time = 0
  
 def main_menu():
     pass
